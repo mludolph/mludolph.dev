eb5a77be3feeaeaa394fb1d6c4329ee349e1e1a9
Moritz Ludolph <mail@moritzludolph.de>
2021-04-02 06:35:48 +0200

    fix broken image

--- components/vcard.vue
+++ components/vcard.vue
@@ -2,7 +2,7 @@
   <div class="flex flex-col sm:flex-row sm:w-content items-center">
     <img
       class="w-24 h-24 mb-4 mr-0 sm:mr-4 sm:mb-0 select-none rounded-full"
-      src="images/profile_square.jpg"
+      src="/images/profile_square.jpg"
       alt="Profile Image"
     />
 
